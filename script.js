449d53c838fd912dabc2edcc11cd707846bde296
prince kumar sacxena <86111082+prince-9211@users.noreply.github.com>
2024-03-28 19:39:57 -0700

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-//your JS code here. If required.
-const counterparagraph=document.getElementById('counter');
-const incrementBtn=documnet.getElementById('Increment Button');
-incrementBtn.addEventListner('click',function() {
-	let currvalue=parseInt(counterparagraph.textContent);
-	alert('un-incremented'+currvalue);
-	currvalue++;
-	counterparagraph.textContent=currvalue;
-});
+var incrementcount=document.getElementById("incrementBtn");
+var counterDisplay=document.getElementById("counter");
+var count=0;
+ function incrementCount() {
+ 	count++;
+	 counterDisplay.textContent=count;
+ }
+//Adding eventListner
+incrementBtn.addEventListener("click",incrementCount);
